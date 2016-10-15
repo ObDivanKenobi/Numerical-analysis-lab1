057c59277ca4d2fa6cceaf57504ab17315c539ea
Ilia Manshin <ilia.manshin@yandex.ru>
2016-10-15 13:28:22 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.textBoxD = new System.Windows.Forms.TextBox();
+            this.buttonBuild = new System.Windows.Forms.Button();
+            this.textBoxMinX = new System.Windows.Forms.TextBox();
+            this.textBoxMaxX = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisX.IsStartedFromZero = false;
+            chartArea1.AxisX.Maximum = 10D;
+            chartArea1.AxisX.Minimum = -10D;
+            chartArea1.AxisY.IsStartedFromZero = false;
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(12, 68);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(954, 584);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(23, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "y = ";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(62, 36);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(38, 20);
+            this.textBoxA.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(106, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "x³ +";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(194, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "x² +";
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(146, 36);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(38, 20);
+            this.textBoxB.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(278, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "x +";
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(234, 36);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(38, 20);
+            this.textBoxC.TabIndex = 2;
+            // 
+            // textBoxD
+            // 
+            this.textBoxD.Location = new System.Drawing.Point(313, 36);
+            this.textBoxD.Name = "textBoxD";
+            this.textBoxD.Size = new System.Drawing.Size(38, 20);
+            this.textBoxD.TabIndex = 2;
+            // 
+            // buttonBuild
+            // 
+            this.buttonBuild.Location = new System.Drawing.Point(357, 29);
+            this.buttonBuild.Name = "buttonBuild";
+            this.buttonBuild.Size = new System.Drawing.Size(75, 33);
+            this.buttonBuild.TabIndex = 3;
+            this.buttonBuild.Text = "Построить!";
+            this.buttonBuild.UseVisualStyleBackColor = true;
+            this.buttonBuild.Click += new System.EventHandler(this.buttonBuild_Click);
+            // 
+            // textBoxMinX
+            // 
+            this.textBoxMinX.Location = new System.Drawing.Point(472, 36);
+            this.textBoxMinX.Name = "textBoxMinX";
+            this.textBoxMinX.Size = new System.Drawing.Size(38, 20);
+            this.textBoxMinX.TabIndex = 2;
+            this.textBoxMinX.Text = "-10";
+            // 
+            // textBoxMaxX
+            // 
+            this.textBoxMaxX.Location = new System.Drawing.Point(582, 36);
+            this.textBoxMaxX.Name = "textBoxMaxX";
+            this.textBoxMaxX.Size = new System.Drawing.Size(38, 20);
+            this.textBoxMaxX.TabIndex = 2;
+            this.textBoxMaxX.Text = "10";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(516, 36);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 20);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "<= x <=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 664);
+            this.Controls.Add(this.buttonBuild);
+            this.Controls.Add(this.textBoxD);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxMaxX);
+            this.Controls.Add(this.textBoxMinX);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart);
+            this.Name = "Form1";
+            this.Text = "Построить!";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.TextBox textBoxD;
+        private System.Windows.Forms.Button buttonBuild;
+        private System.Windows.Forms.TextBox textBoxMinX;
+        private System.Windows.Forms.TextBox textBoxMaxX;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Lab1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBuild_Click(object sender, EventArgs e)
+        {
+            int min = int.Parse(textBoxMinX.Text),
+                max = int.Parse(textBoxMaxX.Text),
+                a, b, c, d;
+            if (textBoxA.Text == "") a = 0;
+            else a = int.Parse(textBoxA.Text);
+            if (textBoxB.Text == "") b = 0;
+            else b = int.Parse(textBoxB.Text);
+            if (textBoxC.Text == "") c = 0;
+            else c = int.Parse(textBoxC.Text);
+            if (textBoxD.Text == "") d = 0;
+            else d = int.Parse(textBoxD.Text);
+
+            Series s = new Series("Default");
+            s.ChartType = SeriesChartType.Spline;
+            List<PointF> source = new List<PointF>();
+            for (int i = min; i <= max; ++i)
+            {
+                float y = (float)(a * Math.Pow(i, 3) + b * Math.Pow(i, 2) + c * i + d);
+                source.Add(new PointF(i, y));
+            }
+
+            chart.Series.Clear();
+            chart.Series.Add(s);
+            chart.ChartAreas[0].AxisX.Minimum = chart.ChartAreas[0].AxisX2.Minimum = min;
+            chart.ChartAreas[0].AxisX.Maximum = chart.ChartAreas[0].AxisX2.Maximum = max;
+
+            chart.DataSource = source;
+            chart.Series[0].XValueMember = "X";
+            chart.Series[0].YValueMembers = "Y";
+            Invalidate();
+        }
+    }
+}
